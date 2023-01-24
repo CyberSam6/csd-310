2d65e34c1a2315f2f10b62a281b5f58d2344c5f7
kropftech <kropftech@gmail.com>
2023-01-23 17:03:12 -0700

    homework

---
+++ module_5/pytech_insert.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@atlascluster.x1bihsw.mongodb.net/pytech"
+client = MongoClient(url)
+students = client.pytech.students
+
+jon = {"student_id": 1007, "first_name": "Jon", "last_name": "smith"}
+freddy = {"student_id": 1008, "first_name": "freddy", "last_name": "lucas"}
+cameron = {"student_id": 1009, "first_name": "Cameron", "last_name": "rob"}
+
+jon_student_id = students.insert_one(jon).inserted_id
+freddy_student_id = students.insert_one(freddy).inserted_id
+cameron_student_id = students.insert_one(cameron).inserted_id
+
