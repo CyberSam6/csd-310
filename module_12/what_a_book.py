6a66dcb138d063f883aae678d1eaced319b7ed7a
kropftech <kropftech@gmail.com>
2023-03-06 17:05:20 -0700

    Update what_a_book.py

--- module_12/what_a_book.py
+++ module_12/what_a_book.py
@@ -3,7 +3,7 @@ import mysql.connector
 from mysql.connector import errorcode
 
 DB_CONFIG = {
-    "user": "whatabook_user",
+    "user": "pysports_user",
     "password": "MySQL8IsGreat!",
     "host": "127.0.0.1",
     "database": "whatabook",
